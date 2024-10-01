6450b2369687be6f1cf9e570aa030c10db150454
abderrahmane <ilahalaha0987@gmail.com>
2024-10-01 12:23:51 +0100

    whyGreenTeam section is responsive

--- src/app/(root)/(routes)/_components/whyGreenTeam.tsx
+++ src/app/(root)/(routes)/_components/whyGreenTeam.tsx
@@ -33,20 +33,23 @@ const tabs: Tab[] = [
 
 const TabsSection = () => {
   return (
-    <div className="flex gap-[33px]">
-      <div className="flex flex-col w-[139px] gap-[55px] ">
+    <div className="flex gap-4 justify-center lg:justify-start lg:gap-[33px]">
+      <div className="flex flex-col w-fit  lg:w-[139px] gap-8 lg:gap-[55px] ">
         {tabs.slice(0, 2).map((tab) => (
-          <div key={tab.title} className="flex flex-col items-center gap-[11px] ">
-            <img src={tab.iconSrc} width={77} />
+          <div
+            key={tab.title}
+            className="flex flex-col  max-w-[200px] lg:max-w-full w-fit lg:w-full items-center gap-[11px] "
+          >
+            <img src={tab.iconSrc} />
             <HeadlineSemibold className="text-center">{tab.title}</HeadlineSemibold>
             <DetailsTypography className="text-center">{tab.description}</DetailsTypography>
           </div>
         ))}
       </div>
-      <div className="flex flex-col w-[139px] gap-[55px] ">
+      <div className="flex flex-col lg:w-[139px]  gap-8 lg:gap-[55px] ">
         {tabs.slice(2).map((tab) => (
-          <div key={tab.title} className="flex flex-col gap-[11px] items-center ">
-            <img src={tab.iconSrc} width={77} />
+          <div key={tab.title} className="flex max-w-[200px] lg:max-w-full flex-col gap-[11px] items-center ">
+            <img src={tab.iconSrc} />
             <HeadlineSemibold className="text-center">{tab.title}</HeadlineSemibold>
             <DetailsTypography className="text-center">{tab.description}</DetailsTypography>
           </div>
@@ -60,7 +63,7 @@ const TabsSection = () => {
 const ParagraphSection = () => {
   return (
     <div className="flex flex-col gap-[44px]">
-      <div className="flex flex-col w-[585px] gap-[11px]">
+      <div className="flex flex-col lg:w-[585px] gap-[11px]">
         <H2 className="text-primaryDefault">
           Daarom kies jij
           <br />
@@ -87,9 +90,9 @@ const ParagraphSection = () => {
 
 export const WhyGreenTeam = () => {
   return (
-    <div className="h-[600px] relative w-full  flex items-center overflow-hidden justify-center">
-      <img src="/home/leaf.svg" className="absolute top-4 -right-5 rotate-[95.81px] " />
-      <div className="flex gap-[91px] relative z-10">
+    <div className=" h-fit px-4 lg:px-0 lg:h-[600px] relative w-full  flex items-center lg:overflow-hidden justify-center">
+      <img src="/home/leaf.svg" className="absolute top-4 max-w-[200px] lg:max-w-full -right-5 rotate-[95.81px] " />
+      <div className="flex gap-[91px] flex-col lg:flex-row relative z-10">
         {/* the section contains the foor tabs */}
         <TabsSection />
         {/* the section contains the title , paragraph and the button */}
