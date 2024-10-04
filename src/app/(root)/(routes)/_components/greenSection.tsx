23d91030d545bf62d4065ee55cd53260062b0625
abderrahmane <ilahalaha0987@gmail.com>
2024-10-04 18:38:19 +0100

    fixing the green section issue

--- src/app/(root)/(routes)/_components/greenSection.tsx
+++ src/app/(root)/(routes)/_components/greenSection.tsx
@@ -2,7 +2,7 @@ import { PrimaryOutlinedBtn } from '@/components/theme/buttons';
 
 export const GreenSection = () => {
   return (
-    <div className="flex relative   bg-green-600 h-fit  py-5 lg:py-0 px-4 flex-col lg:flex-row w-full">
+    <div className="flex relative   bg-green-600 h-fit  py-5 lg:px-0 lg:py-0 px-4 flex-col lg:flex-row w-full">
       <img src="/home/ITPersons.png" className=" w-[25vw] h-fit " />
       <div style={{ width: '80vw' }} className="absolute top-0 right-0   overflow-visible  h-full lg:-ml-[7%]">
         <img className=" hidden w-full h-full  lg:block object-cover" src="/home/greenBg.svg" />
