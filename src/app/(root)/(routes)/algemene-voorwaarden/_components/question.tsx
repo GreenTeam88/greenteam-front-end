8273e8a66ca15d23fd38e378ff3601004d2fe90e
abderrahmane <ilahalaha0987@gmail.com>
2024-10-29 16:54:44 +0100

    removing the voting text

--- src/app/(root)/(routes)/algemene-voorwaarden/_components/question.tsx
+++ src/app/(root)/(routes)/algemene-voorwaarden/_components/question.tsx
@@ -1,15 +1,23 @@
+'use client';
+
+import { useState } from 'react';
+
 import { PrimaryBtn, SecondaryOutlinedBtn } from '@/components/theme/buttons';
 import { BodyText, HeadlineSemibold } from '@/components/theme/typography';
 
 export const TermsConditionPageQuestion = () => {
+  const [infoUsefull, setInfoUsefull] = useState<boolean | null>(null);
   return (
     <div className="flex flex-col items-center gap-[22px] py-[55px] bg-lightOrange w-full">
       <HeadlineSemibold className="text-primaryDefault">Hebben we je probleem kunnen oplossen?</HeadlineSemibold>
       <div className="flex gap-[22px]">
-        <PrimaryBtn>Ja, het was nuttig voor mij</PrimaryBtn>
-        <SecondaryOutlinedBtn>Nee, het was niet nuttig voor mij</SecondaryOutlinedBtn>
+        <PrimaryBtn onClick={() => setInfoUsefull(true)}>Ja, het was nuttig voor mij</PrimaryBtn>
+        <SecondaryOutlinedBtn onClick={() => setInfoUsefull(false)}>
+          Nee, het was niet nuttig voor mij
+        </SecondaryOutlinedBtn>
       </div>
-      <BodyText>We zijn blij dat we je hebben kunnen helpen!</BodyText>
+      {infoUsefull !== null &&
+        (infoUsefull ? <BodyText>We zijn blij dat we je hebben kunnen helpen!</BodyText> : <BodyText>No</BodyText>)}
     </div>
   );
 };
