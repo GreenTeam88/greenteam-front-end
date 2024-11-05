eecfebcc27ecf84d5d8956982a85e2a8b220b0bc
abderrahmane <ilahalaha0987@gmail.com>
2024-11-05 19:14:54 +0100

    ussing prettier

--- src/app/(root)/(routes)/(Parketrenovatie)/Schuren-en-polijsten/_components/secondSection.tsx
+++ src/app/(root)/(routes)/(Parketrenovatie)/Schuren-en-polijsten/_components/secondSection.tsx
@@ -1,33 +1,46 @@
-import { CheckedIcon } from "@/components/icons/check"
-import { PrimaryBtn } from "@/components/theme/buttons"
-import { BodyText, H2, HeadlineSemibold } from "@/components/theme/typography"
+import { CheckedIcon } from '@/components/icons/check';
+import { PrimaryBtn } from '@/components/theme/buttons';
+import { BodyText, H2 } from '@/components/theme/typography';
 
+const listItems: string[] = [
+  'Slijtage van overmatig gebruik',
+  'Extreem veel krasjes ',
+  'Vieze vlekken in de vloer',
+  'Oud-bollige uitstraling',
+];
 
-const listItems : string[]  =["Slijtage van overmatig gebruik" , "Extreem veel krasjes " , "Vieze vlekken in de vloer" , "Oud-bollige uitstraling"]
-
-
-
-const QuestionSection = ()=>{
-    return <div className="flex flex-col gap-11 max-w-[400px]" >
-        <div className="flex flex-col gap-[20px]" >
-        <H2 className="text-primaryDefault" >Ben je het zat om naar die oude vloer te kijken?</H2>
-        <div className="flex flex-col " >
- {listItems.map(item=><div className="flex gap-2 "  ><CheckedIcon/><BodyText>{item}</BodyText></div>)}
-        </div></div>
-        <div className="flex flex-col gap-3">
-            <BodyText className="text-secondaryDefault" >Herkenbaar? Geen zorgen, wij lossen het op!</BodyText>
-            <PrimaryBtn>Offerte berekenen</PrimaryBtn>
-
+const QuestionSection = () => {
+  return (
+    <div className="flex flex-col gap-11 max-w-[400px]">
+      <div className="flex flex-col gap-[20px]">
+        <H2 className="text-primaryDefault">Ben je het zat om naar die oude vloer te kijken?</H2>
+        <div className="flex flex-col ">
+          {listItems.map((item) => (
+            <div key={item} className="flex gap-2 ">
+              <CheckedIcon />
+              <BodyText>{item}</BodyText>
+            </div>
+          ))}
         </div>
+      </div>
+      <div className="flex flex-col gap-3">
+        <BodyText className="text-secondaryDefault">Herkenbaar? Geen zorgen, wij lossen het op!</BodyText>
+        <PrimaryBtn>Offerte berekenen</PrimaryBtn>
+      </div>
     </div>
-}
+  );
+};
 
-export const SecondSection = ()=>{
-    return <div className="flex px-3 flex-col w-full items-center py-20 gap-[33px]" >
-        <p className="text-primaryDefault font-normal" >Parket Renovatie <span className="font-bold" >- Schuren en polijsten</span> </p>
-        <div className="flex flex-col lg:flex-row items-center gap-9 py-14"  >
- <QuestionSection/>
- <img className="max-w-full" src="/Parketrenovatie/image11.png"/>           
-        </div>
+export const SecondSection = () => {
+  return (
+    <div className="flex px-3 flex-col w-full items-center py-20 gap-[33px]">
+      <p className="text-primaryDefault font-normal">
+        Parket Renovatie <span className="font-bold">- Schuren en polijsten</span>{' '}
+      </p>
+      <div className="flex flex-col lg:flex-row items-center gap-9 py-14">
+        <QuestionSection />
+        <img className="max-w-full" src="/Parketrenovatie/image11.png" />
+      </div>
     </div>
-}
+  );
+};
--- src/app/(root)/(routes)/(Parketrenovatie)/Schuren-en-polijsten/_components/whatWaitingFor.tsx
+++ src/app/(root)/(routes)/(Parketrenovatie)/Schuren-en-polijsten/_components/whatWaitingFor.tsx
@@ -1,18 +1,27 @@
-import { BodyText, BodyTextBold } from "@/components/theme/typography"
-import { InfoCardProps, WhiteInfoCard } from "../../../diensten/_components/cards"
+import { BodyText, BodyTextBold } from '@/components/theme/typography';
+import { InfoCardProps, WhiteInfoCard } from '../../../diensten/_components/cards';
 
-const whatWaitingForConfig : InfoCardProps = {
-    title : "Waar wacht je op!" ,
-    paragraphs  :  [ <BodyText key="1" >Als dit is wat je wilt, waar wacht je dan op. Bereken via de knop hieronder wat het kost en neem contact met ons op voor een afspraak. Als je de specialist langs laat komen gaan we bespreken wat voor soort hout u heeft, wat de mogelijkheden zijn, ons advies en we komen met de kleurstalen zodat je zeker weten de juiste beslissing maakt.</BodyText> ,
-<BodyText key="2">Deze afspraak kost eenmalig 50,- euro en deze wordt in mindering gebracht wanneer u de klus aan ons uitbesteed. In heel veel gevallen is deze afspraak niet nodig, en is advies via mailcontact met foto’s voldoende.</BodyText> ,
-<BodyTextBold key="3" className="text-secondaryDefault" > Ik wil dat de parketteur langskomt!</BodyTextBold>
+const whatWaitingForConfig: InfoCardProps = {
+  title: 'Waar wacht je op!',
+  paragraphs: [
+    <BodyText key="1">
+      Als dit is wat je wilt, waar wacht je dan op. Bereken via de knop hieronder wat het kost en neem contact met ons
+      op voor een afspraak. Als je de specialist langs laat komen gaan we bespreken wat voor soort hout u heeft, wat de
+      mogelijkheden zijn, ons advies en we komen met de kleurstalen zodat je zeker weten de juiste beslissing maakt.
+    </BodyText>,
+    <BodyText key="2">
+      Deze afspraak kost eenmalig 50,- euro en deze wordt in mindering gebracht wanneer u de klus aan ons uitbesteed. In
+      heel veel gevallen is deze afspraak niet nodig, en is advies via mailcontact met foto’s voldoende.
+    </BodyText>,
+    <BodyTextBold key="3" className="text-secondaryDefault">
+      {' '}
+      Ik wil dat de parketteur langskomt!
+    </BodyTextBold>,
+  ],
+  imgSrc: '/Parketrenovatie/beautiful-shot-modern-house-staircase.png',
+  buttonText: 'Direct offerte berekenen',
+};
 
-    ] ,
-    imgSrc : "/Parketrenovatie/beautiful-shot-modern-house-staircase.png" ,
-    buttonText : "Direct offerte berekenen"
-}
-
-
-export const WhatWaitingForSection = ()=>{
-return <WhiteInfoCard  {...whatWaitingForConfig}  className="lg:py-16"/>
-}
+export const WhatWaitingForSection = () => {
+  return <WhiteInfoCard {...whatWaitingForConfig} className="lg:py-16" />;
+};
