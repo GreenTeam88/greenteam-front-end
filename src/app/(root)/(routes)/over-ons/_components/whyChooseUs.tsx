0f60e4d4188125e4545e2b48eff3aa02b1123ceb
abderrahmane <ilahalaha0987@gmail.com>
2024-12-06 00:33:58 +0100

    fixing parket issues

--- src/app/(root)/(routes)/over-ons/_components/whyChooseUs.tsx
+++ src/app/(root)/(routes)/over-ons/_components/whyChooseUs.tsx
@@ -1,13 +1,38 @@
+'use client';
+
+import clsx from 'clsx';
+import { Dispatch, SetStateAction, useState } from 'react';
+
 import { StandardCard, StandardCardInfo } from '@/components/cards/infoCards';
 import { BodyText, H2, HeadlineSemibold } from '@/components/theme/typography';
 import { cn } from '@/lib/tailwind';
 
-type IconInfo = { name: string; src: string };
-const iconsInfo: IconInfo[] = [
-  { name: '20 jaar ervaring', src: '/aboutUs/star.svg' },
-  { name: 'Kwaliteit', src: '/aboutUs/done.svg' },
-  { name: 'Duurzaam', src: '/aboutUs/heart.svg' },
-  { name: 'maat', src: '/aboutUs/cards.svg' },
+type TabInfo = { name: string; src: string; paragraph: string };
+const tabsInfo: TabInfo[] = [
+  {
+    name: '20 jaar ervaring',
+    src: '/aboutUs/star.svg',
+    paragraph:
+      'Met twee decennia aan expertise in het renoveren van trappen en vloeren, zijn we uitgegroeid tot een betrouwbare partner voor hoogwaardige interieurprojecten. Onze expertise strekt zich uit over een breed scala aan materialen en stijlen, waardoor we voor elke ruimte de perfecte oplossing kunnen bieden. Dankzij onze jarenlange ervaring kunnen we vakmanschap, precisie en duurzaamheid garanderen bij elk project dat we uitvoeren.',
+  },
+  {
+    name: 'Kwaliteit',
+    src: '/aboutUs/done.svg',
+    paragraph:
+      'Kwaliteit staat bij ons hoog in het vaandel. We streven ernaar om bij elk project de hoogste standaard te bereiken, van de selectie van de beste materialen tot de afwerking met oog voor detail. Ons team van ervaren vakmensen zet zich volledig in om uitmuntend vakwerk te leveren dat voldoet aan de verwachtingen van onze klanten. We geloven dat kwaliteit de basis is van langdurige klanttevredenheid en daarom is dit de kern van alles wat we doen.',
+  },
+  {
+    name: 'Duurzaam',
+    src: '/aboutUs/heart.svg',
+    paragraph:
+      'Duurzaamheid is een belangrijk onderdeel van onze werkwijze. We kiezen bewust voor milieuvriendelijke materialen en technieken die de impact op het milieu minimaliseren. Door duurzame keuzes te maken, dragen we bij aan een betere toekomst en zorgen we ervoor dat onze projecten niet alleen nu, maar ook in de toekomst van waarde blijven. We streven ernaar om verantwoorde keuzes te maken die zowel goed zijn voor onze klanten als voor de planeet. We heten niet voor niets GreenTeam!',
+  },
+  {
+    name: 'Op maat',
+    src: '/aboutUs/cards.svg',
+    paragraph:
+      'Bij GreenTeam geloven we in maatwerk. Ieder project is uniek, en daarom bieden wij oplossingen die precies aansluiten bij de wensen en behoeften van onze klanten. Of het nu gaat om een traprenovatie, een nieuwe vloer, of een complete verbouwing, wij luisteren goed naar uw wensen en vertalen deze naar een perfect passend ontwerp. Met onze flexibele aanpak en oog voor detail zorgen we ervoor dat elk project persoonlijk en uniek is. Uw visie is onze uitdaging, en wij zetten ons in om deze tot in de puntjes te realiseren. Geen standaardoplossingen, maar maatwerk dat volledig aansluit bij uw wensen.',
+  },
 ];
 
 const paragraphs: string[] = [
@@ -27,16 +52,48 @@ const cardsInfos: StandardCardInfo[] = [
   {
     title: 'Behandelingen op maat',
     paragraph:
-      'Er zijn verschillende mogelijkheden voor de afwerking van een vloer na het schuren. Wij zorgen altijd voor de behandeling die het beste past bij de vloer en aansluit op jouw wensen. Er is keuze uit lak, olie of hardwax in verschillende kleuren. Hierbij zullen de gepassioneerde vakmannen van Green Team je altijd adviseren voor het maken van de beste keuze.',
+      'Er zijn verschillende mogelijkheden voor de afwerking van een vloer na het schuren. Wij zorgen altijd voor de behandeling die het beste past bij de vloer en aansluit op jouw wensen. Er is keuze uit lak, olie of hardwax in verschillende kleuren. Hierbij zullen de gepassioneerde vak mannen van Green Team je altijd adviseren voor het maken van de beste keuze.',
     img: '/aboutUs/floorPaperCleaning.png',
   },
 ];
 
-const IconCard: React.FC<IconInfo> = ({ name, src }) => {
+const IconCard: React.FC<TabInfo & { setCurrTab: Dispatch<SetStateAction<string>>; currTab: string }> = ({
+  name,
+  src,
+  currTab,
+  setCurrTab,
+}) => {
+  console.log(currTab, name, currTab === name);
   return (
-    <div className="flex items-center flex-col gap-[22px]">
-      <img src={src} />
+    <div onMouseOver={() => setCurrTab(name)} className={'flex items-center flex-col gap-[22px] cursor-pointer'}>
+      <div
+        className={clsx(
+          'rounded-full border-black10 border-opacity-10   border-2 w-[120px] flex items-center justify-center h-[120px]',
+          {
+            'border-primaryDefault border-opacity-100': currTab == name,
+          }
+        )}
+      >
+        <img src={src} />
+      </div>
       <HeadlineSemibold>{name}</HeadlineSemibold>
+    </div>
+  );
+};
+
+const TabsSection = () => {
+  const [currTab, setCurrTab] = useState(tabsInfo[0].name);
+  return (
+    <div className="flex flex-col items-center px-5 py-28 gap-[55px]">
+      <H2 className="text-primaryDefault">Waarom voor ons kiezen?</H2>
+      <div className="flex gap-[88px] max-w-full flex-wrap  w-full justify-center">
+        {tabsInfo.map((info) => (
+          <IconCard currTab={currTab} setCurrTab={setCurrTab} key={info.name} {...info} />
+        ))}
+      </div>
+      <div className="flex flex-col px-3 lg:px-0 gap-8 lg:w-[798px]">
+        <BodyText className="text-[13px]"> {tabsInfo.find((tabInfo) => tabInfo.name === currTab)?.paragraph}</BodyText>
+      </div>
     </div>
   );
 };
@@ -44,19 +101,7 @@ const IconCard: React.FC<IconInfo> = ({ name, src }) => {
 export const WhyChooseUs = () => {
   return (
     <div className="flex flex-col items-center px-5 py-28 gap-[55px]">
-      <H2 className="text-primaryDefault">Waarom voor ons kiezen?</H2>
-      <div className="flex gap-[88px] max-w-full flex-wrap  w-full justify-center">
-        {iconsInfo.map((info) => (
-          <IconCard key={info.name} {...info} />
-        ))}
-      </div>
-      <div className="flex flex-col px-3 lg:px-0 gap-8 lg:w-[798px]">
-        {paragraphs.map((paragraph) => (
-          <BodyText key={paragraph} className="text-center">
-            {paragraph}
-          </BodyText>
-        ))}
-      </div>
+      <TabsSection />
       <div className="flex py-16 flex-col items-center gap-12 lg:gap-[34px]">
         {cardsInfos.map((cardInfo, index) => (
           <div
