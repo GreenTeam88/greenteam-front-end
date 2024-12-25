236b26781fe8a55913a153a37a222914db257dae
abderrahmane <ilahalaha0987@gmail.com>
2024-12-25 01:25:56 +0100

    adjusting the cards ui on mobile

--- src/app/(root)/(routes)/diensten/_components/cards.tsx
+++ src/app/(root)/(routes)/diensten/_components/cards.tsx
@@ -90,11 +90,11 @@ export const WhiteInfoCard: React.FC<InfoCardProps> = ({
   return (
     <div
       className={cn(
-        'flex w-full py-[99px] p-3 lg:p-0 flex-col lg:flex-row  items-center justify-center gap-[57px]',
+        'flex w-full py-[99px] p-3 lg:p-0 flex-col lg:flex-row  items-center justify-center gap-3 lg:gap-[57px]',
         className
       )}
     >
-      <div className="flex flex-col max-w-[508px]  px-4 lg:px-0 gap-[33px] ">
+      <div className="flex flex-col max-w-[508px]  px-4 lg:px-0 gap lg:gap-[33px] ">
         <div className="flex flex-col gap-[11px] ">
           <h3 className="text-[24px] text-primaryDefault font-semibold leading-[25px] ">{title}</h3>
           <div className="flex flex-col gap-3">
