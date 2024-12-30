111c5499134dc9d29b3057597d4539cbabffff49
abderrahmane <ilahalaha0987@gmail.com>
2024-12-30 15:06:46 +0100

    having a unit ListCard

--- src/app/(root)/(routes)/parketrenovatie/_components/secondSection.tsx
+++ src/app/(root)/(routes)/parketrenovatie/_components/secondSection.tsx
@@ -1,4 +1,5 @@
 import { CheckedIcon } from '@/components/icons/check';
+import { ListCard } from '@/components/listCard';
 import { PrimaryBtnLink } from '@/components/theme/buttons';
 import { BodyText, H2 } from '@/components/theme/typography';
 
@@ -9,28 +10,6 @@ const listItems: string[] = [
   'Oud-bollige uitstraling',
 ];
 
-const QuestionSection = () => {
-  return (
-    <div className="flex flex-col gap-11 max-w-[400px]">
-      <div className="flex flex-col gap-[20px]">
-        <H2 className="text-primaryDefault">Ben je het zat om naar die oude vloer te kijken?</H2>
-        <div className="flex flex-col ">
-          {listItems.map((item) => (
-            <div key={item} className="flex gap-2 ">
-              <CheckedIcon />
-              <BodyText>{item}</BodyText>
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="flex flex-col gap-3">
-        <BodyText className="text-secondaryDefault font-bold">Herkenbaar? Geen zorgen, wij lossen het op!</BodyText>
-        <PrimaryBtnLink href="/offerte">Offerte aanvragen</PrimaryBtnLink>
-      </div>
-    </div>
-  );
-};
-
 type SecondSectionProps = {
   image: string;
   pageName: string;
@@ -39,14 +18,15 @@ type SecondSectionProps = {
 
 export const SecondSection: React.FC<SecondSectionProps> = ({ image, pageName, sectionName }) => {
   return (
-    <div className="flex px-3 flex-col w-full items-center py-6 pb-[150px] gap-[100px]">
-      <p className="text-primaryDefault font-normal">
-        {sectionName + '  '} <span className="font-bold">- {pageName}</span>{' '}
-      </p>
-      <div className="flex flex-col lg:flex-row items-center gap-9 py-14">
-        <QuestionSection />
-        <img className="max-w-full rounded-[10px]" src={image} />
-      </div>
-    </div>
+    <ListCard
+      pageName={pageName}
+      btnLink="/offerte"
+      btnText="Offerte aanvragen"
+      listTitle="Ben je het zat om naar die oude vloer te kijken?"
+      imgSrc={image}
+      sectionName={sectionName}
+      listItems={listItems}
+      orangeText="Herkenbaar? Geen zorgen, wij lossen het op!"
+    />
   );
 };
