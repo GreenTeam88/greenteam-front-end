ce2fdd6d7a03e49e8b51ccf6e872cd60878b6bd6
abderrahmane <ilahalaha0987@gmail.com>
2025-01-09 22:45:07 +0100

    removing unussed declarations

--- src/app/(root)/(routes)/bedankt/page.tsx
+++ src/app/(root)/(routes)/bedankt/page.tsx
@@ -1,5 +1,4 @@
 import { pagesMap } from '@/config/pages';
-import TraprenovatiesPage from '../traprenovatie/page';
 
 function ThankYouPage({ searchParams }: { searchParams: { [key: string]: string } }) {
   return pagesMap[searchParams.page] || pagesMap['/'];
