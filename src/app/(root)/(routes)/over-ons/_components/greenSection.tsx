7b80f7da0fce74881d0dbf2472a779057460e206
abderrahmane <ilahalaha0987@gmail.com>
2025-01-11 11:31:17 +0100

    replacing text

--- src/app/(root)/(routes)/over-ons/_components/greenSection.tsx
+++ src/app/(root)/(routes)/over-ons/_components/greenSection.tsx
@@ -8,19 +8,21 @@ export const GreenSection = () => {
         <img className=" hidden w-full h-full  lg:block object-cover" src="/aboutUs/greenBg.png" />
         <div className="lg:absolute gap-5 py-4 lg:max-w-[70%] relative  lg:py-0 lg:top-1/2 items-center lg:items-start lg:-translate-y-1/2 lg:left-[20%] flex flex-col xl:gap-[33px] lg:gap-[8px]">
           <h3 className="text-xl  max-w-full text-center lg:text-start   lg:text-[1.5vw] text-white lg:leading-[1.5vw]  tracking-[-2%] ">
-            20 jaar ervaring in trap en vloerrenovatieVan eenmanszaak naar{' '}
+            20 jaar ervaring in trap en vloerrenovatie.
+            <br />
+            Van eenmanszaak naar
             <span className="font-semibold">landelijke specialist. </span>{' '}
           </h3>
           <BodyText className="text-white text-center lg:text-start max-w-full">
-            Bij Green Team zetten we ons al 20 jaar lang dag in dag uit in om vloeren een tweede leven te geven. Met al
-            onze ervaring zijn wij dan ook dé specialist voor wat betreft het renoveren van vloeren. Wij zijn
-            uitgegroeid van een eenmanszaak tot een volwaardig bedrijf met vestigingen door heel Nederland. De groei van
-            Green Team komt voort uit de gedachte dat er altijd iets beter kan, wij willen ons graag onderscheiden van
-            andere aanbieders op de markt. Dit doen wij door continu in beweging te blijven en te optimaliseren tijdens
-            het volgen van onze missie om de wereld een stukje groener en beter te maken.
+            Bij GreenTeam zetten we ons al 20 jaar lang dag in dag uit in om vloeren en trappen een tweede leven te
+            geven. Met al onze ervaring zijn wij dan ook dé specialist voor wat betreft het renoveren van vloeren en
+            trappen. Wij zijn uitgegroeid van een eenmanszaak tot een volwaardig bedrijf met vestigingen door heel
+            Nederland. De groei van GreenTeam komt voort uit de gedachte dat er altijd iets beter kan, wij willen ons
+            graag onderscheiden van andere aanbieders op de markt. Dit doen wij door continu in beweging te blijven en
+            te optimaliseren tijdens het volgen van onze missie om de wereld een stukje groener en beter te maken.
           </BodyText>
-          <PrimaryOutlinedBtnLink href="/zakelijk" className="lg:py-2">
-            Dat wil ik!
+          <PrimaryOutlinedBtnLink href="/offerte" className="lg:py-2">
+            Offerte aanvragen{' '}
           </PrimaryOutlinedBtnLink>
         </div>
       </div>
