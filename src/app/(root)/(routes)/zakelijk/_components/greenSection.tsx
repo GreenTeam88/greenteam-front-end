e317b628315f1a8021d0cde5cc39b60834000d09
abderrahmane <ilahalaha0987@gmail.com>
2025-01-12 17:30:17 +0100

    removing unecessary layout

--- src/app/(root)/(routes)/zakelijk/_components/greenSection.tsx
+++ src/app/(root)/(routes)/zakelijk/_components/greenSection.tsx
@@ -5,9 +5,9 @@ export const GreenSection = () => {
     <div className="flex relative lg:justify-end bg-primaryDefault items-center lg:items-start lg:bg-white   h-fit  py-5 lg:px-0 lg:py-0 px-4 flex-col lg:flex-row w-full">
       <div
         style={{ width: 'calc(100vw - 613px)' }}
-        className=" lg:absolute z-20 top-0 left-0 justify-center lg:justify-start flex     overflow-visible  h-full lg:-mr-[7%]"
+        className=" lg:absolute z-20 top-0 min-w-fit left-0 justify-center lg:justify-start flex     overflow-visible  h-full lg:-mr-[7%]"
       >
-        <img className=" hidden w-full h-full  lg:block object-cover" src="/aboutUs/greenBg.png" />
+        <img className=" hidden w-full h-full   lg:block object-cover" src="/aboutUs/greenBg.png" />
         <div className="lg:absolute gap-5 py-4 max-w-[70%] relative  lg:py-0 lg:top-1/2 items-center lg:items-start lg:-translate-y-1/2 lg:left-[20%] flex flex-col xl:gap-[33px] lg:gap-[8px]">
           <h3 className="text-xl   text-center lg:text-start font-bold max-w-[500px]    lg:text-[1.5vw] text-white lg:leading-[37px]  tracking-[-2%] ">
             Wij bieden oplossingen voor trappen en vloeren!{' '}
@@ -18,7 +18,7 @@ export const GreenSection = () => {
         </div>
       </div>
 
-      <div className="items-center z-10 hidden lg:flex relative justify-center   h-[383px]">
+      <div className="items-center  z-10 hidden lg:flex relative justify-center   h-[383px]">
         <img src="/zakelijk/wood.png" className="w-full  h-full " />
       </div>
     </div>
--- src/app/(root)/layout.tsx
+++
@@ -1,5 +0,0 @@
-const SetupLayout = async ({ children }: { children: React.ReactNode }) => {
-  return <> {children} </>;
-};
-
-export default SetupLayout;
