4a04ff933c7919f21542c9a914aff8fe068fd281
abderrahmane <ilahalaha0987@gmail.com>
2025-01-18 23:47:59 +0100

    integraitng the open graph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
     description:
       'GreenTeam is gepassioneerd over vloeren en trappen, met een sterke focus op duurzaamheid. Oorspronkelijk houtbewerkers, hebben we ons gespecialiseerd in het renoveren van vloeren en trappen, waarbij kwaliteit en klanttevredenheid altijd centraal staan. Wij combineren vakmanschap met milieuvriendelijke innovaties, en leveren elke keer weer een resultaat dat onze beloftes overtreft. Bij GreenTeam draait alles om betrouwbaarheid, duurzaamheid, en het respect voor zowel mensen als het milieu.',
     url: 'https://www.greenteam.nl',
-    images: 'https://www.greenteam.nl//favicon.ico',
+    images: 'https://www.greenteam.nl/og-image.jpg',
   },
 };
 
