f7ceca4a9d64e0d5676d3fb4d8cb840aef857ad1
abderrahmane <ilahalaha0987@gmail.com>
2025-02-09 00:24:47 +0100

    updating Veelgestelde vragen text

--- src/app/(root)/(routes)/algemene-voorwaarden/_components/question.tsx
+++ src/app/(root)/(routes)/algemene-voorwaarden/_components/question.tsx
@@ -10,7 +10,7 @@ export const TermsConditionPageQuestion = () => {
   const [infoUsefull, setInfoUsefull] = useState<boolean | null>(null);
   return (
     <div className="flex flex-col items-center gap-[22px] py-[55px] bg-lightOrange w-full">
-      <HeadlineSemibold className="text-primaryDefault">Hebben we je probleem kunnen oplossen?</HeadlineSemibold>
+      <HeadlineSemibold className="text-primaryDefault">Hebben we uw probleem kunnen oplossen?</HeadlineSemibold>
       <div className="flex px-2 lg:px-0 flex-col lg:flex-row gap-3 lg:gap-[22px]">
         <PrimaryBtn className="w-full lg:w-fit" onClick={() => setInfoUsefull(true)}>
           Ja, het was nuttig voor mij
@@ -21,14 +21,14 @@ export const TermsConditionPageQuestion = () => {
       </div>
       {infoUsefull !== null &&
         (infoUsefull ? (
-          <BodyText className="text-center">We zijn blij dat we je hebben kunnen helpen!</BodyText>
+          <BodyText className="text-center">We zijn blij dat we u hebben kunnen helpen!</BodyText>
         ) : (
           <BodyText className="text-center">
-            Neem alstublieft{' '}
+            Neem{' '}
             <Link className="hover:text-primaryDefault active:text-primaryDefault" href="/contact">
               contact
             </Link>{' '}
-            met ons op om het probleem te bespreken.
+            met ons op zodat we u verder kunnen helpen.
           </BodyText>
         ))}
     </div>
