2b6e15ec15c2ac1c708c138f3bfc37ef41e2c768
abderrahmane <ilahalaha0987@gmail.com>
2025-10-16 14:33:48 +0100

    fixing svg issues and updating text

--- src/components/icons/two-arrows-move.tsx
+++ src/components/icons/two-arrows-move.tsx
@@ -4,14 +4,14 @@ export const TwoArrowsMoveIcon = () => {
       <path
         d="M6.5 1L1.78284 5.71716C1.62663 5.87337 1.62663 6.12663 1.78284 6.28284L6.5 11"
         stroke="#195B35"
-        stroke-width="2"
-        stroke-linecap="round"
+        strokeWidth="2"
+        strokeLinecap="round"
       />
       <path
         d="M14.5 1L9.78284 5.71716C9.62663 5.87337 9.62663 6.12663 9.78284 6.28284L14.5 11"
         stroke="#195B35"
-        stroke-width="2"
-        stroke-linecap="round"
+        strokeWidth="2"
+        strokeLinecap="round"
       />
     </svg>
   );
