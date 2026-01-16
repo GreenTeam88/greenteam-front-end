2b5001abb22503f1401a934713af0204cd339b48
abderrahmane <ilahalaha0987@gmail.com>
2026-01-16 23:36:11 +0100

    fixing category section size issue

--- src/app/(root)/(shop-routes)/(shop)/_components/category.tsx
+++ src/app/(root)/(shop-routes)/(shop)/_components/category.tsx
@@ -2,8 +2,8 @@ export const CategorySection = () => {
   return (
     <div className="flex  px-2 lg:px-0 gap-3 w-full items-center py-8 ">
       {' '}
-      <img className="object-contain  " src="/shop/product/home-icon.png" />
-      <p className="text-[9px] lg:text-base"> Tapijten</p>
+      <img className="object-contain  w-[30px]" src="/shop/product/home-icon.png" />
+      <p className="text-[9px] text-xl"> Tapijten</p>
     </div>
   );
 };
