7b824fe7ced53679ff930c07b21b3d8c526e122d
abderrahmane <ilahalaha0987@gmail.com>
2026-01-17 12:43:37 +0100

    integrating the casters modal

---
+++ src/modals/casters.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import { Modal } from '@mui/material';
+import { X } from 'lucide-react';
+import Image from 'next/image';
+
+import { useModalsStore } from '@/store/modals';
+
+export const CastersModal = () => {
+  const { set, 'residential-use': residentialUseModal } = useModalsStore();
+  return (
+    <Modal
+      style={{ backgroundColor: 'transparent' }}
+      open={residentialUseModal}
+      onClose={() => set({ 'residential-use': false })}
+    >
+      <div className="w-[1200px] p-10 max-h-[90vh] overflow-auto rounded-lg bg-white absolute right-1/2 translate-x-1/2 top-1/2 -translate-y-1/2 max-w-[90vw] ">
+        <div className="w-full px-4  py-4 flex border-b border-b-gray-200 items-center justify-end">
+          <div onClick={() => set({ sizeDetailsModal: false })} className="max-w-[100px] relative  max-h-[100px]">
+            <X />
+          </div>
+        </div>
+        <div className="flex px-5  flex-col py-8">
+          <h3 className="text-5xl "> Zwenkwielen</h3>
+          <br />
+          <br />
+          <br />
+          <p>
+            Wanneer de zwenkwielen van bureaustoelen over tapijt rijden kan dit zorgen voor snellere slijtage. Te zachte
+            vloerbedekking is daarom vaak ongeschikt voor gebruik van zwenkwielen.
+          </p>
+          <br />
+          <p>
+            Wilt u gebruikmaken van zwenkwielen, die vooral onder bureaustoelen zitten? Dan is het belangrijk om tapijt
+            te kopen dat hiertegen bestand is.
+          </p>
+          <h3 className="font-semibold text-3xl">Wonen</h3>
+          <br />
+          <Image width={211.99} height={100} alt="house image" src="/modals/trapgeschikt-zwenkwielen-wonen_4.jpg" />
+          <br />
+          <p>
+            Tapijt dat bestand is tegen gebruik van zwenkwielen (bureaustoelen) in huis. Denk daarbij aan de werkkamer
+            in huis.
+          </p>
+          <br />
+          <h3 className="font-semibold text-3xl">Wonen en werken</h3>
+          <br />
+          <Image
+            alt="normaal-woongebruik image"
+            width={211}
+            height={97}
+            src={'/modals/zwenkwielen-wonen-en-werken_3.jpg'}
+          />
+          <br />
+          <p>
+            Tapijt dat bestand is tegen gebruik van zwenkwielen (bureaustoelen) <b>in huis én in bedrijfsruimtes,</b>{' '}
+            zoals bijvoorbeeld kantoren.{' '}
+          </p>
+        </div>
+      </div>
+    </Modal>
+  );
+};
