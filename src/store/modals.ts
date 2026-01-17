84d60fe774da97c816cda0cf1973ef815c5a4f71
abderrahmane <ilahalaha0987@gmail.com>
2026-01-17 16:33:41 +0100

    integrating the new modals in the store

--- src/store/modals.ts
+++ src/store/modals.ts
@@ -4,6 +4,15 @@ type ModalsStore = {
   sizeDetailsModal: boolean;
   set: SetModalsStore;
   'residential-use': boolean;
+  casters: boolean;
+  'comfort-class': boolean;
+  construction: boolean;
+  'dyeing-method': boolean;
+  'pool-height': boolean;
+  'pool-material': boolean;
+  'under-floor-heating': boolean;
+  stairs: boolean;
+  'project-use': boolean;
 };
 
 type SetModalsStore = {
@@ -18,4 +27,13 @@ export const useModalsStore = create<ModalsStore>((set) => ({
   set,
   sizeDetailsModal: false,
   'residential-use': false,
+  casters: false,
+  'comfort-class': false,
+  construction: false,
+  'dyeing-method': false,
+  'pool-height': false,
+  'pool-material': false,
+  'under-floor-heating': false,
+  stairs: false,
+  'project-use': false,
 }));
