5c5d1a9fe97fad5f4e4ca54a01335fe8375dba9d
abderrahmane <ilahalaha0987@gmail.com>
2026-02-08 17:09:41 +0100

    fixing category section ui

--- src/app/(root)/(shop-routes)/(shop)/_components/category.tsx
+++ src/app/(root)/(shop-routes)/(shop)/_components/category.tsx
@@ -2,10 +2,10 @@ import { SmallRightArrow } from '@/components/icons/arrows';
 
 export const CategorySection: React.FC<{ category: string }> = ({ category }) => {
   return (
-    <div className="flex  px-2 lg:px-0 gap-3 w-full items-center py-8 ">
-      <p className="text-[9px] text-xl">home</p>
+    <div className="flex  px-2 lg:px-0 gap-2 w-full items-center py-8 ">
+      <p className="text-xs">home</p>
       <SmallRightArrow />
-      <p className="text-[9px] text-xl"> {category}</p>
+      <p className="text-xs "> {category}</p>
     </div>
   );
 };
