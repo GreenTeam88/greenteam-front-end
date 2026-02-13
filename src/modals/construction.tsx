865b6c450b2292fc1cd5172e20ec5b7c297b22c9
abderrahmane <ilahalaha0987@gmail.com>
2026-02-13 21:40:54 +0100

    fixing other modals issues and adding missing image

--- src/modals/construction.tsx
+++ src/modals/construction.tsx
@@ -9,13 +9,13 @@ export const ConstructionModal = () => {
   const { set, construction } = useModalsStore();
   return (
     <Modal style={{ backgroundColor: 'transparent' }} open={construction} onClose={() => set({ construction: false })}>
-      <div className="w-full lg:w-[1200px] p-10 max-h-[90vh] overflow-auto rounded-lg bg-white absolute right-1/2 translate-x-1/2 top-1/2 -translate-y-1/2 max-w-[90vw] ">
+      <div className="w-full lg:w-[1200px] p-2 lg:p-10 max-h-[90vh] overflow-auto rounded-lg bg-white absolute right-1/2 translate-x-1/2 top-1/2 -translate-y-1/2 max-w-[90vw] ">
         <div className="w-full px-4  py-4 flex border-b border-b-gray-200 items-center justify-end">
           <div onClick={() => set({ construction: false })} className="max-w-[100px] relative  max-h-[100px]">
             <X />
           </div>
         </div>
-        <h3 className="text-5xl "> Constructie</h3>
+        <h3 className="text-3xl lg:text-5xl "> Constructie</h3>
         <br />
         <br />
         <p>Geeft de constructievan de pool aan:</p>
