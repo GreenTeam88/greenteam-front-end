971e4a4f9d37eb3c166a254294e8282271c5c0ce
abderrahmane <ilahalaha0987@gmail.com>
2026-02-28 21:20:50 +0100

    adding missing links

--- src/app/(root)/(shop-routes)/(shop)/_components/category.tsx
+++ src/app/(root)/(shop-routes)/(shop)/_components/category.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link';
+
 import { SmallRightArrow } from '@/components/icons/arrows';
 
 export const CategorySection: React.FC<{ category: string }> = ({ category }) => {
   return (
     <div className="flex  px-2 lg:px-0 gap-2 w-full items-center py-8 ">
-      <p className="text-xs">home</p>
+      <Link href={`/${category}`} className="text-xs">
+        Home
+      </Link>
       <SmallRightArrow />
       <p className="text-xs "> {category}</p>
     </div>
