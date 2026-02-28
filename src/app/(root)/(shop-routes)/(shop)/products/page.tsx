ac176ea9e2bd45057b422f1dfc20b916ab47275d
abderrahmane <ilahalaha0987@gmail.com>
2026-02-28 13:43:50 +0100

    removing unnecessary declarations

--- src/app/(root)/(shop-routes)/(shop)/products/page.tsx
+++ src/app/(root)/(shop-routes)/(shop)/products/page.tsx
@@ -1,15 +1,10 @@
-import { variantsOptionsNames } from '@/config/shop-config';
 import { getAllProducts, getShopifyCollections } from '@/utils/shop/query-tools';
 import { CollectionsSection } from './_components/collections';
 import { AllCarpetsHeader } from './_components/header';
 import { ProductsSection } from './_components/products';
 import { SearchProducts } from './_components/search-products';
 
-export default async function Products({
-  searchParams,
-}: {
-  searchParams?: { [key: string]: string | string[] | undefined };
-}) {
+export default async function Products() {
   const allCollections = await getShopifyCollections();
   const products = await getAllProducts();
 
