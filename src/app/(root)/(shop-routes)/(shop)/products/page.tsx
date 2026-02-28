ae7949c08b4ab7e610a37ff1900bc2ec1c387c20
abderrahmane <ilahalaha0987@gmail.com>
2026-02-28 01:58:14 +0100

    removing unnecessary declarations

--- src/app/(root)/(shop-routes)/(shop)/products/page.tsx
+++ src/app/(root)/(shop-routes)/(shop)/products/page.tsx
@@ -12,19 +12,7 @@ export default async function Products({
 }) {
   const allCollections = await getShopifyCollections();
   const products = await getAllProducts();
-  const colors: string[] = JSON.parse((searchParams?.colors as string) || '[]');
-  const filteredProductsByColor = colors?.length
-    ? products.filter((product) =>
-        product.variants.edges?.some((edge) => {
-          console.log('edge', edge);
-          return edge.node.selectedOptions?.some(
-            (option) =>
-              option.name === variantsOptionsNames.color &&
-              colors.some((color) => color.toLowerCase() === option.value.toLocaleLowerCase())
-          );
-        })
-      )
-    : products;
+
   return (
     <div className="flex px-2 pt-5 max-w-[1440px] flex-col">
       <AllCarpetsHeader />
--- src/components/extension/file-upload.tsx
+++ src/components/extension/file-upload.tsx
@@ -240,7 +240,7 @@ FileUploaderContent.displayName = 'FileUploaderContent';
 
 export const FileUploaderItem = forwardRef<HTMLDivElement, { index: number } & React.HTMLAttributes<HTMLDivElement>>(
   ({ className, index, children, ...props }, ref) => {
-    const { removeFileFromSet, activeIndex } = useFileUpload(); // direction
+    const { activeIndex } = useFileUpload(); // direction
     const isSelected = index === activeIndex;
     return (
       <div
